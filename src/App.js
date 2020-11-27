a73c76f64c76cc62e82668db1b348fd406a2d6cf
Eetu Salli <eetu.salli@gmail.com>
2020-11-27 19:01:20 +0200

    init

--- src/App.js
+++ src/App.js
@@ -4,12 +4,25 @@ function App() {
   return (
     <div style={{ textAlign: 'center' }}>
       <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <h2>On sale</h2>
+      <ul>
+          <li>The escapists 2		4,99</li>
+          <li>Human fall flat 	5,99</li>
+          <li>Vermintide 2 		6,99e</li>
+          <li>Barotrauma 		12,49e</li>
+          <li>Raft			13,39e</li>
+          <li>Unrailed 		14,99e</li>
+        </ul>
+        <h2>Free or owned</h2>
+        <ul>
+          <li>Overwatch</li>
+          <li>Apex Legendsh</li>
+          <li>CS</li>
+          <li>Among Us</li>
+          <li>Armello</li>
+          <li>PUBG =D</li>
+          <li>civ 4,5?</li>
+        </ul>
       </header>
     </div>
   );
